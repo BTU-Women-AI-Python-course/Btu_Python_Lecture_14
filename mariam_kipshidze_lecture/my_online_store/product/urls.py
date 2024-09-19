952eaa94301d70c1447cc54c86c4fe1742db3382
Mariam <mari.kifshidze@gmail.com>
2024-09-19 20:52:54 +0400

    Function-based views

--- mariam_kipshidze_lecture/my_online_store/product/urls.py
+++ mariam_kipshidze_lecture/my_online_store/product/urls.py
@@ -1,4 +1,11 @@
 from django.urls import path
 
+from product.views import product_list, product_detail, product_create, product_update, product_delete
+
 urlpatterns = [
+    path('product_list/', product_list, name='product_list'),
+    path('product_detail/<int:pk>', product_detail, name='product_detail'),
+    path('product_create/', product_create, name='product_create'),
+    path('product_update/<int:pk>', product_update, name='product_update'),
+    path('product_delete/<int:pk>', product_delete, name='product_delete'),
 ]
