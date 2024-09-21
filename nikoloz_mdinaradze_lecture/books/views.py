17d2a333a41d331470abda7a0ff761dab2094c20
NikaMdinaradze <mdinaradzenika04@gmail.com>
2024-09-21 14:17:02 +0400

    add books update view

--- nikoloz_mdinaradze_lecture/books/views.py
+++ nikoloz_mdinaradze_lecture/books/views.py
@@ -25,3 +25,16 @@ def list_books(request):
 def detail_book(request, pk):
     book = get_object_or_404(Book, pk=pk)
     return render(request, "books/books_detail.html", {'book': book})
+
+
+def update_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return HttpResponse("Success !!!")
+    else:
+        form = BookForm(instance=book)
+    return render(request, "books/books_update.html", {'form': form})
+
